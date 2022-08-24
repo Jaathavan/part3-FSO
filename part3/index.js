ed9875054aa049052d866ab017e5fa1cb21e46c3
Jaathavan <rjaathu@outlook.com>
2022-08-24 09:26:01 -0400

    make app use cors

--- part3/index.js
+++ part3/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const morgan = require('morgan')
 
 const app = express()
+const cors = require('cors')
 
 morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
 
 app.use(express.json())
+app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 let persons = [
