fa7cdfb31499b59d8c8d669d7a26812adb57baee
Jaathavan <rjaathu@outlook.com>
2022-08-26 19:16:49 -0400

    require mongoose and extract password from process args

---
+++ mongo.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://jaath_notes:${password}@cluster0.p7nfr0m.mongodb.net/phonebookApp?retryWrites=true&w=majority`
