02fab670401b053147ab7f39b3d277a496af30ef
Jaathavan <rjaathu@outlook.com>
2022-08-26 21:38:52 -0400

    add Schema for person + mongoose model

--- mongo.js
+++ mongo.js
@@ -8,3 +8,11 @@ if (process.argv.length < 3) {
 const password = process.argv[2]
 
 const url = `mongodb+srv://jaath_notes:${password}@cluster0.p7nfr0m.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: Number,
+})
+  
+const Person = mongoose.model('Person', noteSchema)
+  
