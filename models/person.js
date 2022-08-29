b8078e649775af088da142715dd4dad1f29af31f
Jaathavan <rjaathu@outlook.com>
2022-08-29 17:11:37 -0400

    set schema layout to JSON + remove v + export module

--- models/person.js
+++ models/person.js
@@ -14,3 +14,13 @@ const personSchema = new mongoose.Schema({
     name: String,
     number: String,
 })
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Person', personSchema)
