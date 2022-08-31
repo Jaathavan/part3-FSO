7c8abd335b7a4dabf6bc8d2c2de5ec0686876307
Jaathavan <rjaathu@outlook.com>
2022-08-30 21:30:40 -0400

    add post functionality to db + complete 3.14

--- index.js
+++ index.js
@@ -97,15 +97,15 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    const person = {
+    const person = new Person({
         id: generateId(),
         name: body.name,
         number: body.number
-    }
+    })
 
-    persons = persons.concat(person)
-
-    response.json(person)
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
 })
 
 //Listen to PORT
