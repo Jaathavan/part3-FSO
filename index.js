da87bafa697f0e60f42e4938752ff1f133a6034b
Jaathavan <rjaathu@outlook.com>
2022-08-31 11:37:53 -0400

    add error handling + complete 3.16

--- index.js
+++ index.js
@@ -80,6 +80,25 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+  
+app.use(unknownEndpoint)
+  
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+}
+  
+// this has to be the last loaded middleware.
+app.use(errorHandler)
+
 //Listen to PORT
 const PORT = process.env.PORT
 app.listen(PORT, () => {
