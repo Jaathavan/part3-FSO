588c826c3854711ec3839ec58415cea9986995f5
Jaathavan <rjaathu@outlook.com>
2022-08-31 11:16:55 -0400

    complete 3.15 + add delete functionality

--- index.js
+++ index.js
@@ -13,44 +13,11 @@ app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 app.use(cors())
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 //Displays persons in JSON on api/persons
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
         response.json(persons)
     })
-})
-
-//Displays number of people in phonebook on /info
-app.get('/info', (request, response) => {
-    response.send(
-        `
-        <div>Phonebook has info for ${persons.length} people</div>
-        ${new Date()}
-        `
-    )
 })
 
 //Fetch single source route
@@ -62,43 +29,39 @@ app.get('/api/persons/:id', (request, response) => {
 
 //DELETE resource
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    
-    response.status(204).end()
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
-
-const generateId = () => {
-    return Math.floor(Math.random() * 100000)
-}
 
 //POST new resource
 app.post('/api/persons', (request, response) => {
     const body = request.body
     
-    if (!body.name) {
+    if (body.name === undefined) {
         return response.status(400).json({
             error: 'name is missing'
         })
     }
-    else if (!body.number) {
+    else if (body.number === undefined) {
         return response.status(400).json({
             error: 'number is missing'
         })
     }
-    else if (persons.find(person => person.name === body.name)) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-    else if (persons.find(person => person.number === body.number)) {
-        return response.status(400).json({
-            error: 'number must be unique'
-        })
-    }
+    // else if (Person.find({ name: body.name })) {
+    //     return response.status(400).json({
+    //         error: 'name must be unique'
+    //     })
+    // }
+    // else if (Person.find({ number: body.number })) {
+    //     return response.status(400).json({
+    //         error: 'number must be unique'
+    //     })
+    // }
 
     const person = new Person({
-        id: generateId(),
         name: body.name,
         number: body.number
     })
