f3e7cb80137e0710446c58c027b4493ebac3106b
Jaathavan <rjaathu@outlook.com>
2022-08-31 21:28:57 -0400

    complete 3.21

--- models/person.js
+++ models/person.js
@@ -23,7 +23,7 @@ const personSchema = new mongoose.Schema({
             validator: function(v) {
                 return /^\d{2,3}-\d{6,}/.test(v);
             },
-            message: props => `${props.value} is not a valid phone number`
+            message: props => `${props.value} is not a valid phone number!`
         },
         required: true
     }
