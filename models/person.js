01baf19a323f8899d1decbfc9c1906ad12c574b5
Jaathavan <rjaathu@outlook.com>
2022-09-01 08:52:34 -0400

    add eslint from airbnb guide

--- models/person.js
+++ models/person.js
@@ -1,40 +1,40 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
 mongoose.connect(url)
   .then(() => {
-    console.log("Connected to MongoDB")
+    console.log('Connected to MongoDB');
   })
   .catch((error) => {
-    console.log('Error connecting to MongoDB:', error.message)
-  })
+    console.log('Error connecting to MongoDB:', error.message);
+  });
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d{6,}/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
     },
-    number: {
-        type: String,
-        minLength: 8,
-        validate: {
-            validator: function(v) {
-                return /^\d{2,3}-\d{6,}/.test(v);
-            },
-            message: props => `${props.value} is not a valid phone number!`
-        },
-        required: true
-    }
-})
+    required: true
+  }
+});
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-})
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+});
 
 module.exports = mongoose.model('Person', personSchema)
